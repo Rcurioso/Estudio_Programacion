f6a54bdbca7997b79b55504177ce3e4aa704da2d
Rcurioso <rubengguk@gmail.com>
2023-09-29 10:23:38 +0200

    Primer cambio

---
+++ decoradores.py
@@ -0,0 +1,7 @@
+def decorador(funcion):
+    def funcion_modificada():
+        print("Antes de llamar a la función")
+        funcion()
+        print("Después de llamar a la función")
+    return funcion_modificada
+
